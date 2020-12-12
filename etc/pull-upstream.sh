c6cf15cb46a6678085f1f5bdf60bd441830574c2
Boris Serebrov <serebrov@gmail.com>
2020-12-12 11:46:18 +0200

    Create tags when updating from upstream

--- etc/pull-upstream.sh
+++ etc/pull-upstream.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+git tag custom/$(date "+%Y-%m-%d-%H-%M")-before
+git push origin --tags
+
 git checkout master
 git fetch upstream
 git pull upstream master
@@ -9,3 +12,6 @@ git checkout custom
 
 git merge master
 git push origin HEAD
+
+git tag custom/$(date "+%Y-%m-%d-%H-%M")-after
+git push origin --tags
