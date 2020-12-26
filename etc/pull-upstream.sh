ae754b98e14bc4ec98cf32a43e6a975c9d4618a8
Boris Serebrov <serebrov@gmail.com>
2020-12-26 18:58:29 +0200

    Do not automatically push custom branch after upstream pull.
    This way it is easier to review the changes.

--- etc/pull-upstream.sh
+++ etc/pull-upstream.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 git tag custom/$(date "+%Y-%m-%d-%H-%M")-before
-git push origin --tags
 
 git checkout master
 git fetch upstream
@@ -11,7 +10,8 @@ git push origin master
 git checkout custom
 
 git merge master
-git push origin HEAD
 
 git tag custom/$(date "+%Y-%m-%d-%H-%M")-after
-git push origin --tags
+
+# git push origin HEAD
+# git push origin --tags
