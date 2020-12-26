3a8f8bacf0800159f1395ac3965c91e7121cc5ef
Boris Serebrov <serebrov@gmail.com>
2020-12-27 00:27:17 +0200

    Fix microdox flash scripts

--- etc/qmk-flash-microdox-elite-c.sh
+++ etc/qmk-flash-microdox-elite-c.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-qmk flash -kb boardsource/microdox -km cole -bl dfu
+# See https://beta.docs.qmk.fm/developing-qmk/qmk-reference/config_options#setting-handedness
+#
+# I use dfu-split-right to explicitly set the elite-c part to be right
+# and pro-micro to be left (in ./qmk-flash-microdox-pro-micro.sh)
+# Otherwise they are mirrored (right half produces qwert and left - yuiop)
+qmk flash -kb boardsource/microdox -km cole -bl dfu-split-right
