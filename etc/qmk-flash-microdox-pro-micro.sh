7ef2885112efbe37022f7ff16efa03f013ab848f
Boris Serebrov <serebrov@gmail.com>
2020-12-27 00:27:17 +0200

    Fix microdox flash scripts

--- etc/qmk-flash-microdox-pro-micro.sh
+++ etc/qmk-flash-microdox-pro-micro.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-qmk flash -kb boardsource/microdox -km cole
+# See https://beta.docs.qmk.fm/developing-qmk/qmk-reference/config_options#setting-handedness
+#
+# I use avrdude-split-left to explicitly set the pro micro part to be left
+# and elite-c to be right (in ./qmk-flash-microdox-elite-c.sh)
+# Otherwise they are mirrored (right half produces qwert and left - yuiop)
+qmk flash -kb boardsource/microdox -km cole -bl avrdude-split-left
