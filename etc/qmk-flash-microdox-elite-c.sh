7d3a286eb9c61085993202986bccf55bcb607f39
Boris Serebrov <serebrov@gmail.com>
2020-12-28 18:01:36 +0200

    Add microdox-mini keymap

--- etc/qmk-flash-microdox-elite-c.sh
+++ etc/qmk-flash-microdox-elite-c.sh
@@ -5,4 +5,4 @@
 # I use dfu-split-right to explicitly set the elite-c part to be right
 # and pro-micro to be left (in ./qmk-flash-microdox-pro-micro.sh)
 # Otherwise they are mirrored (right half produces qwert and left - yuiop)
-qmk flash -kb boardsource/microdox -km cole -bl dfu-split-right
+qmk flash -kb boardsource/microdox -km microdox-mini -bl dfu-split-right
