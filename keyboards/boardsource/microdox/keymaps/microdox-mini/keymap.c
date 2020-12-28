020e3451f7fdbf265554c2d0063bf67ef3e665ad
Boris Serebrov <serebrov@gmail.com>
2020-12-28 18:37:53 +0200

    Update the layout

--- keyboards/boardsource/microdox/keymaps/microdox-mini/keymap.c
+++ keyboards/boardsource/microdox/keymaps/microdox-mini/keymap.c
@@ -20,42 +20,84 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 enum layers {
     _QWERTY,
-    _RAISE,
-    _LOWER,
-    _ADJUST
+    _SYM,
+    _NAV,
+    _FN,
+    _NUM,
+    _MOUSE
 };
 
 #define KC_CTSC RCTL_T(KC_SCLN)
-#define LOWER_SPC LT(_LOWER, KC_SPC)
-#define RASE_BACK LT(_RAISE, KC_BSPC)
+/* #define LOWER_SPC LT(_LOWER, KC_SPC) */
+/* #define RASE_BACK LT(_RAISE, KC_BSPC) */
 #define SHFT_Z MT(MOD_LSFT, KC_Z)
 #define SHIFT_SLASH MT(MOD_RSFT,KC_SLSH)
-#define RASE_ENT LT(_RAISE, KC_ENT)
+/* #define RASE_ENT LT(_RAISE, KC_ENT) */
 #define CTRL_A MT(MOD_LCTL,KC_A)
 #define PREVWINDOW MT(MOD_LCTL, KC_LEFT)
 #define NEXTWINDOW MT(MOD_LCTL, KC_RGHT)
 
 
 
+/* const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {*/
+/*   [_QWERTY] = LAYOUT_split_3x5_3(*/ 
+/*         KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,             KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,*/  
+/*       CTRL_A,    KC_S,    KC_D,    KC_F,    KC_G,             KC_H,    KC_J,    KC_K,    KC_L, KC_CTSC,*/ 
+/*      SHFT_Z,    KC_X,    KC_C,    KC_V,    KC_B,             KC_N,    KC_M, KC_COMM,  KC_DOT, SHIFT_SLASH,*/ 
+/*                        MO(_LOWER),KC_LGUI, RASE_ENT,       RASE_BACK, LOWER_SPC, KC_TAB*/
+/*     ),*/
+/*   [_RAISE] = LAYOUT_split_3x5_3(*/ 
+/*         KC_1,    KC_2,    KC_3,    KC_4,    KC_5,           KC_6,    KC_7,    KC_8,    KC_9,    KC_0,*/ 
+/*       KC_ESC, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,            KC_LEFT, KC_DOWN,   KC_UP, KC_RGHT, KC_QUOT,*/ 
+/*      KC_LSFT, KC_GRV, PREVWINDOW, NEXTWINDOW, XXXXXXX,           KC_MINS,  KC_EQL, KC_LBRC, KC_RBRC, KC_BSLS,*/ 
+/*                          _______, KC_LGUI, KC_ENT,           KC_BSPC,  KC_SPC, _______*/
+/*                          ),*/
+/*   [_LOWER] = LAYOUT_split_3x5_3(*/ 
+/*        KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,             KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN,*/
+/*        KC_ESC,   XXXXXXX, XXXXXXX, XXXXXXX, RGB_TOG,            KC_LEFT, KC_DOWN,   KC_UP, KC_RGHT, KC_DQT,*/ 
+/*        KC_ESC,   KC_TILD, PREVWINDOW, NEXTWINDOW, RGB_MODE_FORWARD,   KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE,*/ 
+/*                             _______,  KC_LGUI,  KC_ENT,         KC_BSPC,  KC_SPC, _______*/
+/*                             )*/
+/* };*/
+
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-  [_QWERTY] = LAYOUT_split_3x5_3( 
-        KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,             KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,  
-      CTRL_A,    KC_S,    KC_D,    KC_F,    KC_G,             KC_H,    KC_J,    KC_K,    KC_L, KC_CTSC, 
-     SHFT_Z,    KC_X,    KC_C,    KC_V,    KC_B,             KC_N,    KC_M, KC_COMM,  KC_DOT, SHIFT_SLASH, 
-                       MO(_LOWER),KC_LGUI, RASE_ENT,       RASE_BACK, LOWER_SPC, RGB_TOG /*KC_TABi*/
+	[_QWERTY] = LAYOUT_split_3x5_3(
+        KC_Q, KC_W, KC_E, KC_R, KC_T,                                   KC_Y, KC_U, KC_I, KC_O, KC_P, 
+        KC_A, KC_S, KC_D, KC_F, KC_G,                                   KC_H, KC_J, KC_K, KC_L, RGUI_T(KC_ENT), 
+        KC_Z, KC_X, KC_C, KC_V, KC_B,                                   KC_N, KC_M, LALT_T(KC_COMM), KC_DOT, KC_SLSH, 
+            OSM(MOD_LCTL), OSL(1), LT(2,KC_SPC),                        LT(5,KC_SPC), OSL(1), OSM(MOD_RSFT)
     ),
-  [_RAISE] = LAYOUT_split_3x5_3( 
-        KC_1,    KC_2,    KC_3,    KC_4,    KC_5,           KC_6,    KC_7,    KC_8,    KC_9,    KC_0, 
-      KC_ESC, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,            KC_LEFT, KC_DOWN,   KC_UP, KC_RGHT, KC_QUOT, 
-     KC_LSFT, KC_GRV, PREVWINDOW, NEXTWINDOW, XXXXXXX,           KC_MINS,  KC_EQL, KC_LBRC, KC_RBRC, KC_BSLS, 
-                         _______, KC_LGUI, KC_ENT,           KC_BSPC,  KC_SPC, _______
-                         ),
-  [_LOWER] = LAYOUT_split_3x5_3( 
-       KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,             KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN,
-       KC_ESC,   XXXXXXX, XXXXXXX, XXXXXXX, RGB_TOG,            KC_LEFT, KC_DOWN,   KC_UP, KC_RGHT, KC_DQT, 
-       KC_ESC,   KC_TILD, PREVWINDOW, NEXTWINDOW, RGB_MODE_FORWARD,   KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE, 
-                            _______,  KC_LGUI,  KC_ENT,         KC_BSPC,  KC_SPC, _______
-                            )
+	[_SYM] = LAYOUT_split_3x5_3(
+        KC_1, KC_2, KC_3, KC_4, KC_5,                                   KC_6, KC_7, KC_8, KC_9, KC_0,
+        KC_GRV, KC_LPRN, KC_RPRN, LT(4,KC_QUOT), KC_EQL,                KC_BSLS, LT(3,KC_MINS), KC_LBRC, KC_RBRC, KC_SCLN, 
+        KC_TILD, KC_LT, KC_GT, KC_DQUO, KC_PLUS,                        KC_PIPE, KC_UNDS, KC_LCBR, KC_RCBR, KC_COLN, 
+            KC_TRNS, KC_TRNS, KC_BSPC,                                  KC_BSPC, KC_TRNS, KC_TRNS
+    ),
+	[_NAV] = LAYOUT_split_3x5_3(
+        RGUI(KC_1), RGUI(KC_2), RGUI(KC_3), RGUI(KC_4), RGUI(KC_5),     KC_HOME, KC_PGUP, KC_PGDN, KC_END, KC_NO, 
+        KC_NO, KC_NO, RGUI(KC_O), KC_TAB, KC_ESC,                       KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, KC_NO, 
+        KC_NO, KC_NO, KC_NO, KC_DEL, KC_CAPS,                           LALT(KC_LEFT), KC_NO, KC_NO, RALT(KC_RGHT), KC_NO, 
+                  KC_TRNS, KC_TRNS, KC_TRNS,                            KC_BSPC, KC_TRNS, KC_TRNS
+    ),
+	[_FN] = LAYOUT_split_3x5_3(
+        KC_F1, KC_F2, KC_F3, KC_F4, KC_F5,                              KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, 
+        KC_F6, KC_F7, KC_F8, KC_F9, KC_F10,                             KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, 
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_F11, KC_F12,                      KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, 
+                      KC_TRNS, KC_TRNS, KC_TRNS,                        KC_TRNS, KC_TRNS, KC_TRNS
+    ),
+	[_NUM] = LAYOUT_split_3x5_3(
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                    KC_PAST, KC_P7, KC_P8, KC_P9, KC_PSLS, 
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                    KC_PMNS, KC_P4, KC_P5, KC_P6, KC_TRNS, 
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                    KC_PEQL, KC_P1, KC_P2, KC_P3, KC_PPLS, 
+                      KC_TRNS, KC_TRNS, KC_TRNS,                        KC_TRNS, KC_P0, KC_PDOT
+    ),
+	[_MOUSE] = LAYOUT_split_3x5_3(
+        RGB_TOG, RGB_MODE_FORWARD, KC_WH_D, KC_NO, KC_NO,               KC_NO, KC_NO, KC_MS_U, KC_NO, KC_PSCR, 
+        KC_NO, KC_WH_L, KC_WH_U, KC_WH_R, KC_NO,                        KC_NO, KC_MS_L, KC_MS_D, KC_MS_R, KC_NO, 
+        KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,                              KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, 
+              KC_TRNS, KC_BTN1, KC_BTN2,                                KC_TRNS, KC_TRNS, KC_TRNS
+    )
 };
 
 #ifdef OLED_DRIVER_ENABLE
@@ -82,21 +124,36 @@ static void render_status(void) {
       oled_write_P(PSTR("^\n"), false);
       oled_write_P(PSTR("Layer: Base\n"), false);
       break;
-    case _RAISE:
+    case _SYM:
       oled_write_P(PSTR("B R L A O\n"), false);
       oled_write_P(PSTR("  ^\n"), false);
-      oled_write_P(PSTR("Layer: Raise\n"), false);
+      oled_write_P(PSTR("Layer: Sym\n"), false);
       break;
-    case _LOWER:
+    case _NAV:
       oled_write_P(PSTR("B R L A O\n"), false);
       oled_write_P(PSTR("    ^\n"), false);
-      oled_write_P(PSTR("Layer: Lower\n"), false);
+      oled_write_P(PSTR("Layer: Nav\n"), false);
       break;
-    case _ADJUST:
+    case _FN:
       oled_write_P(PSTR("B R L A O\n"), false);
       oled_write_P(PSTR("      ^\n"), false);
-      oled_write_P(PSTR("Layer: Adjust\n"), false);
+      oled_write_P(PSTR("Layer: FN\n"), false);
       break;
+    case _NUM:
+      oled_write_P(PSTR("B R L A O\n"), false);
+      oled_write_P(PSTR("      ^\n"), false);
+      oled_write_P(PSTR("Layer: Num\n"), false);
+      break;
+    case _MOUSE:
+      oled_write_P(PSTR("B R L A O\n"), false);
+      oled_write_P(PSTR("      ^\n"), false);
+      oled_write_P(PSTR("Layer: Mouse\n"), false);
+      break;
+    /* case _ADJUST: */
+    /*   oled_write_P(PSTR("B R L A O\n"), false); */
+    /*   oled_write_P(PSTR("      ^\n"), false); */
+    /*   oled_write_P(PSTR("Layer: Adjust\n"), false); */
+    /*   break; */
     default:
       oled_write_P(PSTR("B R L A O"), false);
       oled_write_P(PSTR("        ^\n"), false);
