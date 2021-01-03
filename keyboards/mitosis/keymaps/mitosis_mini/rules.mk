2070777ba569e5f7733441ce087cb115987a2034
Boris Serebrov <serebrov@gmail.com>
2021-01-03 15:31:17 +0200

    Tried combos, do not work with autoshift

--- keyboards/mitosis/keymaps/mitosis_mini/rules.mk
+++ keyboards/mitosis/keymaps/mitosis_mini/rules.mk
@@ -1,3 +1,8 @@
 SRC += custom.c
 
 AUTO_SHIFT_ENABLE = yes
+
+# Enable combos, see https://docs.qmk.fm/#/feature_combo?id=combos
+# Combos do not work with autoshift
+# https://github.com/qmk/qmk_firmware/issues/8136
+# COMBO_ENABLE=yes
