90faf06f14f26939ca11985bb0b11b2aad2e82f9
Boris Serebrov <serebrov@gmail.com>
2021-01-03 15:54:43 +0200

    Reduce firmware size

--- keyboards/mitosis/keymaps/mitosis_mini/rules.mk
+++ keyboards/mitosis/keymaps/mitosis_mini/rules.mk
@@ -6,3 +6,15 @@ AUTO_SHIFT_ENABLE = yes
 # Combos do not work with autoshift
 # https://github.com/qmk/qmk_firmware/issues/8136
 # COMBO_ENABLE=yes
+
+AUDIO_ENABLE = no # audio output
+FAUXCLICKY_ENABLE = no
+BOOTMAGIC_ENABLE = no	# Virtual DIP switch configuration(+1000)
+MOUSEKEY_ENABLE = no	# Mouse keys(+4700)
+EXTRAKEY_ENABLE = yes	# Audio control and System control(+450)
+CONSOLE_ENABLE = no	# Console for debug(+400)
+COMMAND_ENABLE = no	# Commands for debug and configuration
+UNICODE_ENABLE = no	# Unicode
+BACKLIGHT_ENABLE = no	# Enable keyboard backlight functionality
+BLUETOOTH_ENABLE = no	# Enable Bluetooth with the Adafruit EZ-Key HID
+MIDI_ENABLE = no	# MIDI controls
