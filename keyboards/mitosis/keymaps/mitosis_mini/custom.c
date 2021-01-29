4c3dedd590810f6924bec91d801363c3bdca84d9
Boris Serebrov <serebrov@gmail.com>
2021-01-29 19:29:17 +0200

    Additional combos.

--- keyboards/mitosis/keymaps/mitosis_mini/custom.c
+++ keyboards/mitosis/keymaps/mitosis_mini/custom.c
@@ -23,16 +23,31 @@ enum mitosis_layers
 enum combo_events {
   JK_ESC,
   DF_TAB,
+  QW_SFT,
+  /* SD_LAYER, */
+  /* KL_LAYER, */
+  SD_CTRL,
+  KL_CTRL,
   COMBO_LENGTH
 };
 uint16_t COMBO_LEN = COMBO_LENGTH;
 
 const uint16_t PROGMEM jk_combo[] = {KC_J, KC_K, COMBO_END};
 const uint16_t PROGMEM df_combo[] = {KC_D, KC_F, COMBO_END};
+const uint16_t PROGMEM qw_combo[] = {KC_Q, KC_W, COMBO_END};
+const uint16_t PROGMEM sd_combo[] = {KC_S, KC_D, COMBO_END};
+const uint16_t PROGMEM kl_combo[] = {KC_K, KC_L, COMBO_END};
 
 combo_t key_combos[] = {
   [JK_ESC] = COMBO(jk_combo, KC_ESC),
-  [DF_TAB] = COMBO(df_combo, KC_TAB)
+  [DF_TAB] = COMBO(df_combo, KC_TAB),
+  [QW_SFT] = COMBO(qw_combo, KC_RSFT),
+  [SD_CTRL] = COMBO(sd_combo, KC_LCTL),
+  [KL_CTRL] = COMBO(kl_combo, KC_RCTL),
+  /* [QW_SFT] = COMBO(qw_combo, OSM(KC_RSFT)), */
+  /* [SD_LAYER] = COMBO(sd_combo, MO(_SYM)), */
+  /* [SD_LAYER] = COMBO(sd_combo, OSL(_SYM)), */
+  /* [KL_LAYER] = COMBO(kl_combo, OSL(_SYM)), */
 };
 
 
