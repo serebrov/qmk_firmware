ea078bbc4ebcf64dc1bba8821c12b8e41787a403
Boris Serebrov <serebrov@gmail.com>
2021-02-24 02:13:22 +0200

    Add custom Planck keymap

---
+++ keyboards/planck/keymaps/planck-mini/rules.mk
@@ -0,0 +1,10 @@
+SRC += muse.c
+SRC += custom.c
+
+AUTO_SHIFT_ENABLE = yes
+
+# Settings are in keyboards/planck/rev6/rules.mk
+# AUDIO_ENABLE = yes # audio output
+# BACKLIGHT_ENABLE = yes	# Enable keyboard backlight functionality
+# MIDI_ENABLE = yes	# MIDI controls
+# MOUSEKEY_ENABLE = yes	# Mouse keys(+4700)
