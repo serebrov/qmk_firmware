6ee914ceb3bdd7d23eec98e2a69d142edee3bb32
Boris Serebrov <serebrov@gmail.com>
2021-03-01 02:34:31 +0200

    Add kyria keymap and helper scripts

---
+++ etc/qmk-flash-kyria-left-elite-c.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# See https://beta.docs.qmk.fm/developing-qmk/qmk-reference/config_options#setting-handedness
+#
+# I use avrdude-split-left to explicitly set the pro micro part to be left
+# and elite-c to be right (in ./qmk-flash-microdox-elite-c.sh)
+# Otherwise they are mirrored (right half produces qwert and left - yuiop)
+qmk flash -kb kyria/rev1 -km kyria-mini -bl dfu-split-left
